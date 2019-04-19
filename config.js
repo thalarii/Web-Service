72cc1f6e8226137281fff34d063fda82603708ce
Endashaw Demsis <endashawdemsis@gmail.com>
2019-04-19 15:57:41 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  ENV: process.env.NODE_ENV || "development",
+  PORT: process.env.PORT || 3000,
+  URL: process.env.BASE_URL || "http://localhost:3000",
+  MONGODB_URI:
+    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/check_out_order_list",
+  JWT_SECRET: process.env.JWT_SECRET || "secret1"
+};
---
+++ index.js
@@ -0,0 +1,23 @@
+const restify = require("restify");
+const mongoose = require("mongoose");
+const config = require("./config");
+
+const server = restify.createServer();
+
+//Middleware
+server.use(restify.plugins.bodyParser());
+server.use(restify.plugins.queryParser());
+
+server.listen(config.PORT, () => {
+  mongoose.set("useFindAndModify", false);
+  mongoose.connect(config.MONGODB_URI, {
+    useNewUrlParser: true
+  });
+});
+
+const db = mongoose.connection;
+db.on("error", err => console.log(err));
+
+db.once("open", () => {
+  console.log(`Server started on port ${config.PORT}`);
+});
