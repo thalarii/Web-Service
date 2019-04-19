ec9968975d6f5011edfd273ca07a1a76fc7c088f
abiyabiy36 <49784864+abiyabiy36@users.noreply.github.com>
2019-04-19 07:58:53 -0400

    Authorization rule have been added to the routes

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 server.use(restify.plugins.queryParser());
 
+server.use(
+  rjwt({ secret: config.JWT_SECRET }).unless({
+    path: ["/auth", "/register"]
+  })
+);
+
+
 server.listen(config.PORT, () => {
   mongoose.set("useFindAndModify", false);
   mongoose.connect(config.MONGODB_URI, {
