ae181267807d8cd5161d5cad6cc8296952d0e509
filimonkd <40231410+filimonkd@users.noreply.github.com>
2019-05-31 13:49:02 +0300

    updating mongodb configeration to firebase

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  ENV: process.env.NODE_ENV || "development",
-  PORT: process.env.PORT || 3000,
-  URL: process.env.BASE_URL || "http://localhost:3000",
-  MONGODB_URI:
-    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/check_out_order_list",
-  JWT_SECRET: process.env.JWT_SECRET || "secret1"
-};
+    ENV: process.env.NODE_ENV || 'development',
+    PORT: process.env.PORT || 5000,
+    URL: process.env.BASE_URL || 'http://localhost:5000',
+  };
+  
