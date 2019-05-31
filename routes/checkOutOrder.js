f27a46cd3a51b3ac7121d71830fc470c23b08938
filimonkd <40231410+filimonkd@users.noreply.github.com>
2019-05-31 13:57:32 +0300

    mongodb syntax changed to firbase

--- routes/checkOutOrder.js
+++ routes/checkOutOrder.js
@@ -1,90 +1,82 @@
-const errors = require("restify-errors");
-const CheckOutOrder = require("../models/CheckOutOrder");
-const randomize = require("randomatic");
+const errors = require('restify-errors');
+var admin = require("firebase-admin");
 
+var db = admin.database();
+var ref = db.ref("customer");
 module.exports = server => {
-  // store checkoutorder
-  server.post("/checkOutOrder", async (req, res, next) => {
-    if (!req.is("application/json")) {
-      return next(new errors.InvalidContentError("Expects 'application/json"));
-    }
-
-    const { items, account } = req.body;
-    const unique_id = randomize("Aa0", 256);
-    //   if (validate(items)) {
-    let invalidAttriutes = [];
-    for (let i = 0; i < items.length; i++) {
-      const element = items[i];
-      if (
-        element.item_price == "" ||
-        element.item_name == "" ||
-        element.item_quantity == "" ||
-        element.item_image_link == ""
-      ) {
-        invalidAttriutes.push(element);
+server.post(
+    '/customers',
+    // rjwt({ secret: config.JWT_SECRET }),
+    async (req, res, next) => {
+      // Check for JSON
+      if (!req.is('application/json')) {
+        return next(
+          new errors.InvalidContentError("Expects 'application/json'")
+        );
       }
-    }
 
-    if (account.owner_name == "" || account.account_number == "") {
-      return next(new errors.InvalidContentError("Account unverified"));
-    }
-    // console.log(invalidAttriutes);
-
-    const checkOutOrder = new CheckOutOrder({
-      items,
-      account,
-      is_purchased: false
+      const { 
+        items:{
+          item_name,
+          item_price,
+          item_quality,
+          item_image_link
+        },
+        owner:{
+          owner_name,
+          owner_account
+        },
+        isPurchased
+       } = req.body;
+      var usersRef = ref.child("1");
+    usersRef.set({
+    items: {
+        item_name,
+        item_price,
+        item_quality,
+        item_image_link
+    },
+    owner: {
+        owner_name,
+        owner_account
+    },
+    isPurchased
     });
-    if (invalidAttriutes.length == 0) {
       try {
-        const newCheckOutOrder = await checkOutOrder.save();
-        res.send(newCheckOutOrder._id);
+        // const newCustomer = await customer.save();
+        
+        res.send(201);
         next();
       } catch (err) {
         return next(new errors.InternalError(err.message));
       }
     }
-  });
+  );
+  
+  server.put(
+    '/customers',
+    // rjwt({ secret: config.JWT_SECRET }),
+    async (req, res, next) => {
+      // Check for JSON
+      if (!req.is('application/json')) {
+        return next(
+          new errors.InvalidContentError("Expects 'application/json'")
+        );
+      }
 
-  // search checkoutorder
-  server.get("/checkOutOrder/:id", async (req, res, next) => {
-    try {
-      const checkOutOrder = await CheckOutOrder.findById(req.params.id);
-      res.send(checkOutOrder);
-      next();
-    } catch (error) {
-      return next(
-        new errors.ResourceNotFoundError(
-          `The id ${
-            req.params.id
-          } either outdated or not found on server, please check your id.`
-        )
-      );
+      const { 
+        isPurchased
+       } = req.body;
+var usersRef = ref.child("1");
+    usersRef.set({
+    isPurchased
+    });
+      try {
+           res.send(201);
+        next();
+      } catch (err) {
+        return next(new errors.InternalError(err.message));
+      }
     }
-  });
-
-  // update is_purchased attribute of a given element
-  server.put("/checkOutOrder/:id", async (req, res, next) => {
-    // Check for JSON
-    if (!req.is("application/json")) {
-      return next(new errors.InvalidContentError("Expects 'application/json"));
-    }
-
-    try {
-      const checkOutOrder = await CheckOutOrder.findOneAndUpdate(
-        { _id: req.params.id },
-        req.body
-      );
-      res.send(200);
-      next();
-    } catch (err) {
-      return next(
-        new errors.ResourceNotFoundError(
-          `The id ${
-            req.params.id
-          } either outdated or not found on server, please check your id.`
-        )
-      );
-    }
-  });
+  );
 };
